35e1094b4259746dd009828f9784b17535255704
Ronald Tse <ronald.tse@ribose.com>
2026-05-27 16:18:09 +0800

    Rename namespace: IsoIec80000Namespace → IsqNamespace
    - Rename isoiec80000_namespace.rb → isq_namespace.rb
    - Change prefix: isoiec80000 → isq
    - Change URI: /standards/isoiec80000/ → /standards/isq/
    - Update autoload in namespaces.rb
    - Update spec expectations

--- lib/sdu_smart/rdf/namespaces.rb
+++ lib/sdu_smart/rdf/namespaces.rb
@@ -5,7 +5,7 @@ module SduSmart
     module Namespaces
       autoload :SmartNamespace, "#{__dir__}/namespaces/smart_namespace"
       autoload :OaNamespace, "#{__dir__}/namespaces/oa_namespace"
-      autoload :IsoIec80000Namespace, "#{__dir__}/namespaces/isoiec80000_namespace"
+      autoload :IsqNamespace, "#{__dir__}/namespaces/isq_namespace"
       autoload :DcatNamespace, "#{__dir__}/namespaces/dcat_namespace"
       autoload :ProvNamespace, "#{__dir__}/namespaces/prov_namespace"
       autoload :SkosXlNamespace, "#{__dir__}/namespaces/skosxl_namespace"
---
+++ lib/sdu_smart/rdf/namespaces/isq_namespace.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module SduSmart
+  module Rdf
+    module Namespaces
+      class IsqNamespace < Lutaml::Rdf::Namespace
+        uri "https://w3id.org/standards/isq/ontologies/core/"
+        prefix "isq"
+      end
+    end
+  end
+end
